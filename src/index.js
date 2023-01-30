1e4c7fdeb28c594edb8d1a6144295b91a30c7419
ronaldvic <ronaldblancobalboa@gmail.com>
2023-01-30 13:13:27 -0400

    login added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { createServer } = require('http');
 const realtimeServer = require('./realtimeServer')
+const cookieParser = require('cookie-parser')
 const path = require('path');
 
 const app = express();
@@ -9,6 +10,7 @@ const httpServer = createServer(app);
 //Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname,'views'))
+app.use(cookieParser());
 
 //routes
 app.use( require('./routes') );
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,13 @@ const path = require('path');
 
 const views = path.join(__dirname,"/../views");
 
-router.get('/',(req,res)=>{
+const isLoggendIn = require('../middlewares/isLoggedIn');
+
+router.get('/',isLoggendIn,(req,res)=>{
     res.sendFile(views + '/index.html')
+});
+router.get('/register',(req,res)=>{
+    res.sendFile(views + "/register.html");
 })
 
 module.exports = router;
